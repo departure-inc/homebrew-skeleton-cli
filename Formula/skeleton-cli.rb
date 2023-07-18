444789d8625d8e0eb42a2f959d67a3f69376981e
runeleaf <runeleaf@gmail.com>
2023-07-18 18:54:54 +0900

    update

--- Formula/skeleton-cli.rb
+++ Formula/skeleton-cli.rb
@@ -2,7 +2,7 @@ class SkeletonCli < Formula
   desc "This is the skeleton command for the initial project file deployment."
   homepage "https://github.com/departure-inc/homebrew-skeleton-cli"
   url "https://github.com/departure-inc/skeleton-cli/releases/download/v0.1.0/skeleton"
-  sha256 "9fa21f177d023aefa481f066977ed745f4090ab3e37632b8e91aade253e40c1f"
+  sha256 "db8ac0fc7c7ef9e90c5de2b9d29459484dc01f15391610318d6a8ed320dc632b"
   license "MIT"
 
   def install
